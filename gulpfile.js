587df66f1d0191846453c1437b8c8ca64ddb5483
Jamie Evans <jamie.evans@mastercard.com>
2018-04-11 12:38:01 +0100

    =added lighthouse gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,14 @@ const http = require('http');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 
-gulp.task('webserver', function () {
+let config = {
+  min_scores: {
+    performance: 50,
+    accessibility: 50
+  }
+};
+
+gulp.task('startServer', function () {
   var stream = gulp.src('app')
     .pipe(webserver({
       livereload: false,
@@ -21,6 +28,41 @@ gulp.task('webserver', function () {
     }));
 });
 
-gulp.task('webserver-stop', function (cb) {
+gulp.task('lighthouse', ['startServer'], function (cb) {
+  chromeLauncher.launch({chromeFlags: ['--headless']}).then(function (chrome) {
+    lighthouse(
+      `http://localhost:8000`,
+      {port: chrome.port},// available options - https://github.com/GoogleChrome/lighthouse/#cli-options
+      {
+        "extends": "lighthouse:default",
+        "settings": {
+          "onlyCategories": ["performance", "accessibility"]
+        }
+      }
+    ).then(function (results) {
+      chrome.kill().then(function () {
+        var errors = [];
+        results.reportCategories.forEach(cat => {
+          console.log(cat.id, cat.score, `(${config.min_scores[cat.id]})`, cat.score > config.min_scores[cat.id]);
+          if (config.min_scores[cat.id] && cat.score < config.min_scores[cat.id]) {
+            errors.push(`Failure: Score for ${cat.id} (${cat.score}) is under the allowed score`);
+          }
+        });
+        //output the html report using lighthouse's cli printer
+        require('./node_modules/lighthouse/lighthouse-cli/printer').write(results, 'html', 'lighthouse-report.html');
+        if (errors.length) {
+          cb(errors);
+        } else {
+          cb();
+        }
+        gulp.start('stopServer');
+      })
+    }).catch(function (e) {
+      cb(e);
+      gulp.start('stopServer');
+    });
+  });
+});
+gulp.task('stopServer', function (cb) {
   http.request('http://localhost:8000/_kill_').on('close', cb).end();
 });
